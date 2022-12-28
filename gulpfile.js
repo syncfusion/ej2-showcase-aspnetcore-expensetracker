f2f8f89bee29c2c5a4dd0cc83267363a89cb9ed0
Prasanth Madhaiyan <prasanth.madhaiyan@syncfusion.com>
2022-12-28 19:10:13 +0530

    feature(EJ2-66059): Removed all the files.

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require("gulp");
-module.exports = gulp;
-var window = {};
-var fs = require('fs');
-var elasticlunr = require('elasticlunr');
-var beautify = require('json-beautify');
-require("@syncfusion/ej2-staging");
